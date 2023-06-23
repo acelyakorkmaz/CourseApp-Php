7f45695f9120fd8b0b0c52a5abde6676bdd623ba
acelyakorkmaz <acelyakorkmaz61@gmail.com>
2023-06-23 19:24:51 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.2.3/css/bootstrap.min.css" integrity="sha512-SbiR/eusphKoMVVXysTKG/7VseWii+Y3FdHrt0EpKgpToZeemhqHeZeLWLhJutz/2ut2Vw1uQEj2MbRF+TVBUA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+</head>
+<body>
+
+<nav class="navbar navbar-expend-lg bg-primary navbar-dark">
+    <a href="/index.php" class="navbar-brand">Course App</a>
+    <ul class="navbar-nav me-auto">
+        <li class="nav-item">
+            <a href="#" class="nav-link">Anasayfa</a>
+        </li>
+        <li class="nav-item">
+            <a href="#" class="nav-link">Kurslar</a>
+        </li>
+    </ul>
+
+</nav>
+    
+</body>
+</html>
