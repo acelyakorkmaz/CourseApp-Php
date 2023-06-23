358ea28e84d69e5a462eb568abecdae3395c19c2
acelyakorkmaz <acelyakorkmaz61@gmail.com>
2023-06-24 00:23:38 +0300

    list and cards

--- index.php
+++ index.php
@@ -1,29 +1,86 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.2.3/css/bootstrap.min.css" integrity="sha512-SbiR/eusphKoMVVXysTKG/7VseWii+Y3FdHrt0EpKgpToZeemhqHeZeLWLhJutz/2ut2Vw1uQEj2MbRF+TVBUA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap/5.2.3/css/bootstrap.min.css" />
 
 </head>
+
 <body>
-
-<nav class="navbar navbar-expend-lg bg-primary navbar-dark">
-    <a href="/index.php" class="navbar-brand">Course App</a>
-    <ul class="navbar-nav me-auto">
-        <li class="nav-item">
-            <a href="#" class="nav-link">Anasayfa</a>
-        </li>
-        <li class="nav-item">
-            <a href="#" class="nav-link">Kurslar</a>
-        </li>
-        <li class="nav-item ">
-            <a href="#" class="nav-link"></a>
-        </li>
-    </ul>
-
+<nav class="navbar navbar-expand-lg bg-primary navbar-dark">
+  <a class="navbar-brand" href="#">CourseApp</a>
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav mr-auto">
+      <li class="nav-item active">
+        <a class="nav-link" href="#"> Anasayfa <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#">Kurslar</a>
+      </li>
+     
+  </div>
 </nav>
-    
+<div class="cointainer my-3">
+<div class="row">
+<div class="col-3">
+<div class="list-group">
+
+  <a href="#" class="list-group-item list-group-item-action">Algoritma</a>
+  <a href="#" class="list-group-item list-group-item-action">Web Programlama</a>
+  <a href="#" class="list-group-item list-group-item-action">Mobil Uygulama</a>
+ 
+
+  </div>
+  </div>
+<div class="col-9">
+    <div class="card mb-3">
+        
+    <div class="row">
+        <div class="col-md-4">
+            <img src="img/1.jpg"  class="img-fluid rounded-start">
+        </div>
+        <div class="col-md-8">
+            <div class="card-body">
+            <h5 class="card-title">Web Geliştime</h5>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officia illo animi nostrum, ducimus laudantium doloribus sequi tempora beatae accusamus quidem in porro debitis?</p>
+        </div>
+    </div>
+    </div>
+    </div>
+    <div class="card mb-3">
+        
+    <div class="row">
+        <div class="col-md-4">
+            <img src="img/2.jpg"  class="img-fluid rounded-start">
+        </div>
+        <div class="col-md-8">
+            <div class="card-body">
+            <h5 class="card-title">Phyton Kursu</h5>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officia illo animi nostrum, ducimus laudantium doloribus sequi tempora beatae accusamus quidem in porro debitis?</p>
+        </div>
+    </div>
+    </div>
+    </div>
+    <div class="card mb-3">
+        
+    <div class="row">
+        <div class="col-md-4">
+            <img src="img/3.jpg"  class="img-fluid rounded-start">
+        </div>
+        <div class="col-md-8">
+            <div class="card-body">
+            <h5 class="card-title">Javascript Kursu</h5>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officia illo animi nostrum, ducimus laudantium doloribus sequi tempora beatae accusamus quidem in porro debitis?</p>
+        </div>
+    </div>
+    </div>
+    </div>
+</div>
+</div>
+</div>
 </body>
+
 </html>
